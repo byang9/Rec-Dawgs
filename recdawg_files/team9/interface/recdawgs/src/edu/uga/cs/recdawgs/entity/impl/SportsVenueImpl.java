7c632fff3cc43f0b6394e8e3074c671ac4af7fde
Logan Jahnke <ljahnke9@gmail.com>
2016-04-09 14:47:35 -0400

    added SportsVenueImpl.java, updated HasVenue in Persistence

---
+++ recdawg_files/team9/interface/recdawgs/src/edu/uga/cs/recdawgs/entity/impl/SportsVenueImpl.java
@@ -0,0 +1,58 @@
+package edu.uga.cs.recdawgs.entity.impl;
+
+import edu.uga.cs.recdawgs.persistence.Persistable;
+import edu.uga.cs.recdawgs.entity.SportsVenue;
+
+public interface SportsVenue implements SportsVenue extends Persistable {
+    
+    private String name;
+    private boolean isIndoor;
+    private String address;
+    
+    /** Return the name of this sports venue.
+     * @return the name of this sports venue
+     */
+    public String getName() {
+        return name;
+    }
+    
+    
+    /** Set the new address for this sports venue.
+     * @param name the new address for this sports venue
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    /** Return the indoor status of this sports venue.
+     * @return the indoor status of this sports venue
+     */
+    public boolean getIsIndoor() {
+        return isIndoor;
+    }
+    
+    
+    /** Set the new indoor status for this sports venue.
+     * @param isIndoor the new indoor status for this sports venue
+     */
+    public void setIsIndoor(boolean isIndoor) {
+        this.isIndoor = isIndoor;
+    }
+    
+    
+    /** Return the address of this sports venue.
+     * @return the address of this sports venue
+     */
+    public String getAddress() {
+        return address;
+    }
+    
+    
+    /** Set the new address for this sports venue
+     * @param address the new address of this sports venue
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
