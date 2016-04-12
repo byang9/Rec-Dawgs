ed2f5365d9a7415429d73ae2c49b72aa5954548f
justintumale <fjetumale@yahoo.com>
2016-04-12 00:05:57 -0400

    TeamManager implemented

--- recdawg_files/SE_Engineering/team9/RECDAWGS/interface/recdawgs/src/edu/uga/cs/recdawgs/persistence/impl/TeamManager.java
+++ recdawg_files/SE_Engineering/team9/RECDAWGS/interface/recdawgs/src/edu/uga/cs/recdawgs/persistence/impl/TeamManager.java
@@ -22,10 +22,9 @@ public class TeamManager(){
     public TeamManager(Connection conn, ObjectLayer objectLayer){
 	this.conn = conn;
 	this.objectLayer = objectLayer;
-    }
-
-    //TODO throws Exception                                                                                                                             
-    public void save(Team team){
+    }//constructor
+                                                                                                                           
+    public void save(Team team) throws RDException{
         String               insertTeamSql = 
         "insert into team ( name, leagueId, established, captainId) 
         values ( ?, ?, ?, ? )";              
@@ -101,10 +100,10 @@ public class TeamManager(){
             e.printStackTrace();
             throw new ClubsException( "TeamManager.save: failed to save a Team: " + e );
         }
-    }
+    }//save
 
     public Iterator<Team> restore(Team team) throws RDException{
-        String selectTeamSql = "select id, username, userpass, email, firstname, lastname, address, phone from person";
+        String selectTeamSql = "select id, name, leagueId, established, captainId";
         Statement stmt = null;
         StringBuffer query = new StringBuffer( 100 );
         StringBuffer condition = new StringBuffer( 100 );
@@ -113,11 +112,77 @@ public class TeamManager(){
 
         query.append ( selectTeamSql );
 
-    }
+        if (modelTeam != null){
+            if (modelTeam.getId() >= 0)
+                query.append( " where id = " + modelTeam.getId()); //team id is unique, so it is sufficient to get team
+           
+            else if (modelTeam.getTeamName() != null) //team name is unique, so it is sufficient to get team
+                query.append( " where team name = '" + modelTeam.getTeamName() + "'");
+           
+            else{
+                if (modelTeam.getLeagueId != null)
+                    condition.append( " leagueId = '" + modelTeam.getLeagueId() + "'");
+
+                if (modelTeam.getEstablishedOn() != null)
+                    condition.append( " established = '" + modelTeam.getEstablishedOn()+ "'");
+
+                if (modelTeam.captainId() != null)
+                    condition.append( " captainId = '" + modelTeam.getCaptainId()+ "'");
+
+
+                if( condition.length() > 0 ) {
+                    query.append(  " where " );
+                    query.append( condition );
+                }
+
+            }
+
+        }
+
+        try {
+            stmt = conn.createStatement();
+
+            if (stmt.execute(query.toString())){
+                ResultSet r = stmt.getResultSet();
+                return new TeamIterator(r, objectLayer);
+            }
+
+        }
+        catch(Exception e){
+            throw new RDException( "TeamManager.restore: Could not restore persistent Team object; Root cause: " + e);
+        }
+        throw new RDException("TeamManager.restore: Could not restore persistent Team object")
+
+    }//restore
 
     public void delete(Team team){
+        String               deleteTeamSql = "delete from team where id = ?";              
+        PreparedStatement    stmt = null;
+        int                  inscnt;
+        
+        // form the query based on the given Team object instance
+        if( !team.isPersistent() ) // is the Team object persistent?  If not, nothing to actually delete
+            return;
+        
+        try {
+            
+            //DELETE t1, t2 FROM t1, t2 WHERE t1.id = t2.id;
+            //DELETE FROM t1, t2 USING t1, t2 WHERE t1.id = t2.id;
+            stmt = (PreparedStatement) conn.prepareStatement( deleteTeamSql );
+            
+            stmt.setLong( 1, team.getId() );
+            
+            inscnt = stmt.executeUpdate();
+            
+            if( inscnt == 0 ) {
+                throw new RDException( "PersonManager.delete: failed to delete this Person" );
+            }
+        }
+        catch( SQLException e ) {
+            throw new RDException( "PersonManager.delete: failed to delete this Person: " + e.getMessage() );
+        }
 
-    }
+    }//delete
 
 
 }
